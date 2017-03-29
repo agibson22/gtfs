6d90fcc96673f199665c1cec3fe867c1ca9dbfa4
Alex Gibson <agibson22@gmail.com>
2017-03-28 20:18:41 -0400

    extracting from the feed

--- gtfs_rt_extract.rb
+++ gtfs_rt_extract.rb
@@ -7,11 +7,22 @@ gtfs_rt_url = "http://api.octa.net/GTFSRealTime/protoBuf/VehiclePositions.aspx"
 
 data = Net::HTTP.get(URI.parse(gtfs_rt_url))
 feed = Transit_realtime::FeedMessage.decode(data)
-for entity in feed.entity do
-	if entity.field?(:vehicle)
-		p "Trip ID: #{entity.vehicle.trip.trip_id}"
-		p "Vehicle ID: #{entity.vehicle.vehicle.id}"
-		p "Current Position: #{entity.vehicle.position.latitude}, #{entity.vehicle.position.longitude}, #{entity.vehicle.position.bearing}"
-		# p entity.vehicle.position
+p feed.entity.count
+p feed.entity.first.field?(:vehicle)
+feed.entity[0..10].each do |v|
+
+	if v.field?(:vehicle)
+		p v.vehicle.position.longitude
 	end
 end
+# for entity in feed.entity do
+# 	if entity.field?(:vehicle)
+# 		p "Trip ID: #{entity.vehicle.trip.trip_id}"
+# 		p "Vehicle ID: #{entity.vehicle.vehicle.id}"
+# 		p "Current Position: #{entity.vehicle.position.latitude},
+# 			#{entity.vehicle.position.longitude},
+# 			#{entity.vehicle.position.bearing}"
+# 		# p entity.vehicle.position
+# 		p entity.vehicle
+# 	end
+# end
