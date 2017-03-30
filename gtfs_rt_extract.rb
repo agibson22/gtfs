529aa0b45d5ca0de143df7d4c43eb0883c0fdc8e
Alex Gibson <agibson22@gmail.com>
2017-03-30 18:35:53 -0400

    adding info to hashes

--- gtfs_rt_extract.rb
+++ gtfs_rt_extract.rb
@@ -7,14 +7,27 @@ gtfs_rt_url = "http://api.octa.net/GTFSRealTime/protoBuf/VehiclePositions.aspx"
 
 data = Net::HTTP.get(URI.parse(gtfs_rt_url))
 feed = Transit_realtime::FeedMessage.decode(data)
-p feed.entity.count
-p feed.entity.first.field?(:vehicle)
+vehicle_hashes = []
 feed.entity[0..10].each do |v|
-
 	if v.field?(:vehicle)
-		p v.vehicle.position.longitude
+		vehicle_info = { 
+			"id" => v.vehicle.vehicle.id,
+			"lat" => v.vehicle.position.latitude,
+			"long" => v.vehicle.position.longitude
+			}
+		vehicle_hashes << vehicle_info
 	end
 end
+p vehicle_hashes.size
+
+
+
+
+
+
+
+
+
 # for entity in feed.entity do
 # 	if entity.field?(:vehicle)
 # 		p "Trip ID: #{entity.vehicle.trip.trip_id}"
