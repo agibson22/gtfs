cf73171912b760a4dc753ac379064ed221fa63b9
Alex Gibson <agibson22@gmail.com>
2017-03-30 22:14:56 -0400

    converting files to csv for data extraction:

--- gtfs_extract.rb
+++ gtfs_extract.rb
@@ -1,7 +1,8 @@
 require 'rubygems'
 require 'zip'
+require 'smarter_csv'
 
-gtfs_file = 'google_bus.zip'
+gtfs_file = 'gtfs.zip'
 
 # build directory in /feeds
 directory_name = "feeds/#{gtfs_file}"
@@ -12,12 +13,35 @@ Zip::File.open(gtfs_file) do |zip_file|
 	zip_file.each do |entry|
 		# extract to file/directory/symlink
 		puts "Extracting #{entry.name}"
-		#replace with agency.name
-		entry.extract("feeds/#{gtfs_file}/#{entry.name}")
+		# replace with agency.name
+		entry.extract("#{directory_name}/#{entry.name}") unless File.exists?("#{directory_name}/#{entry.name}")
 	end
 end
 
-# save entries to db?
+# # make it a csv
+# File.rename("#{directory_name}/routes.txt", "#{directory_name}/routes.csv") unless File.exists?("#{directory_name}/routes.csv")
+
+# # get ish from the csv
+# csv_routes = "#{directory_name}/routes.csv"
+# routes = SmarterCSV.process(csv_routes)
+
+# # finding a route by id
+# p routes.find { |x| x[:route_id] == 57 }
+
+# make it a csv
+File.rename("#{directory_name}/shapes.txt", "#{directory_name}/shapes.csv") unless File.exists?("#{directory_name}/shapes.csv")
+
+# get ish from the csv
+csv_shapes = "#{directory_name}/shapes.csv"
+shapes = SmarterCSV.process(csv_shapes)
+
+# finding a route by id
+# p shapes.find { |x| x[:shape_id] == 110 }
+# oof gonna need to db this crap
+
+
+
+
 # make top level directory the name of the agency
 # create text file for vehicles
 # create text file for vehicle assignments
--- gtfs_rt_extract.rb
+++ gtfs_rt_extract.rb
@@ -7,35 +7,13 @@ gtfs_rt_url = "http://api.octa.net/GTFSRealTime/protoBuf/VehiclePositions.aspx"
 
 data = Net::HTTP.get(URI.parse(gtfs_rt_url))
 feed = Transit_realtime::FeedMessage.decode(data)
-vehicle_hashes = []
-feed.entity[0..10].each do |v|
-	if v.field?(:vehicle)
-		vehicle_info = { 
-			"id" => v.vehicle.vehicle.id,
-			"lat" => v.vehicle.position.latitude,
-			"long" => v.vehicle.position.longitude
-			}
-		vehicle_hashes << vehicle_info
+for entity in feed.entity do
+	if entity.field?(:vehicle)
+		p "Trip ID: #{entity.vehicle.trip.trip_id}"
+		p "Vehicle ID: #{entity.vehicle.vehicle.id}"
+		p "Current Position: #{entity.vehicle.position.latitude},
+			#{entity.vehicle.position.longitude},
+			#{entity.vehicle.position.bearing}"
+		p entity.vehicle.trip.route_id
 	end
 end
-p vehicle_hashes.size
-
-
-
-
-
-
-
-
-
-# for entity in feed.entity do
-# 	if entity.field?(:vehicle)
-# 		p "Trip ID: #{entity.vehicle.trip.trip_id}"
-# 		p "Vehicle ID: #{entity.vehicle.vehicle.id}"
-# 		p "Current Position: #{entity.vehicle.position.latitude},
-# 			#{entity.vehicle.position.longitude},
-# 			#{entity.vehicle.position.bearing}"
-# 		# p entity.vehicle.position
-# 		p entity.vehicle
-# 	end
-# end
